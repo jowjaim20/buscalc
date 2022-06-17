4567eebb7df0283bb471db72f23699975f7d5445
Joel Jaim <jowjaim20@gmail.com>
2022-06-17 10:54:46 +0800

    test

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,10 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-start h-screen">
+    <div className="flex flex-col justify-start h-screen text-center">
       <Header />
-      <main className="mb-auto">main</main>
+
+      <main className="mb-auto text-rose-500 uppercase shadow-md">Joel cute kaayo</main>
       <Navigation />
     </div>
   );
