52c88adb8e2805d500862df9d7caf0da6fa6e2fd
Luie John Lumanta <luiejohn.24@gmail.com>
2022-06-23 12:39:06 +0800

    more height

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,11 @@ import HomePageLiability from "./components/HomePageLiability";
 import { CardWrapper } from "./components/wrappers/wrappers";
 import { getAssets, toggleAddAsset } from "./features/assets/assetsSlice";
 import { tooggleAdd } from "./features/liabilities/liabilitiesSlice";
-
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
 
 export default function Home() {
   const { liabilities } = useSelector((state) => state.liabilities);
@@ -13,7 +17,7 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-start mb-auto text-center">
       <CardWrapper>
-        <h2 className="mb-2 font-semibold uppercase text-white">Liabilities</h2>
+        <h2 className="mb-2 font-semibold text-white uppercase">Liabilities</h2>
         {liabilities.length > 0 ? (
           <section>
             {liabilities.map((liability) => (
@@ -21,22 +25,24 @@ export default function Home() {
             ))}
           </section>
         ) : (
-          <ButtonLink href="/liabilities" toggle={tooggleAdd}>Add</ButtonLink>
+          <ButtonLink href="/liabilities" toggle={tooggleAdd}>
+            Add
+          </ButtonLink>
         )}
       </CardWrapper>
       <CardWrapper>
-        <h2 className="mb-2 font-semibold uppercase text-white">Assets</h2>
+        <h2 className="mb-2 font-semibold text-white uppercase">Assets</h2>
 
         {assets.length > 0 ? (
           <section>
             {assets.map((singleAsset) => (
               <HomePageAsset key={singleAsset.id} asset={singleAsset} />
             ))}
-
-            
           </section>
         ) : (
-          <ButtonLink href="/assets" toggle={toggleAddAsset}>Add</ButtonLink>
+          <ButtonLink href="/assets" toggle={toggleAddAsset}>
+            Add
+          </ButtonLink>
         )}
       </CardWrapper>
     </main>
